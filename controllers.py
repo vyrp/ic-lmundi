7f1e494babb7e2cc69985026c0f255b1d1a5736c
Felipe Pereira <felipe.croata@gmail.com>
2016-10-27 05:24:42 -0200

    Sample table htmls and form htmls, 404 and 500 htmls, all controllers stubs.

--- controllers.py
+++ controllers.py
@@ -1,15 +1,81 @@
 import jinja2
+import logging
 import os
+import traceback
 import webapp2
 
-jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'])
+jinja = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=["jinja2.ext.autoescape"],
+    autoescape=True
+)
 
-def render(template, values):
+def render(template, values={}):
     return jinja.get_template(template).render(values)
 
 def handler_404(request, response, exception):
-    response.write(render('templates/404.html', {}))
+    logging.exception(exception)
+    response.write(render("templates/404.html"))
+    response.set_status(404)
+
+def handler_500(request, response, exception):
+    logging.exception(exception)
+    response.write(render("templates/500.html", {"stack_trace": traceback.format_exc().strip()}))
+    response.set_status(500)
+
+class ClassesHandler(webapp2.RequestHandler):
+    def get(self):
+        values = {
+            "active": "turmas",
+        }
+        self.response.write(render("templates/alunos.html", values))
+
+class ClassHandler(webapp2.RequestHandler):
+    def get(self, id):
+        values = {
+            "active": "turmas",
+        }
+        self.response.write(render("templates/aluno.html", values))
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(render('templates/home.html', {}))
+        values = {
+            "active": "home",
+        }
+        self.response.write(render("templates/home.html", values))
+
+class SettingsHandler(webapp2.RequestHandler):
+    def get(self):
+        values = {
+            "active": "configuracoes",
+        }
+        self.response.write(render("templates/configuracoes.html", values))
+
+class StudentHandler(webapp2.RequestHandler):
+    def get(self, id):
+        values = {
+            "active": "alunos",
+            "id": id
+        }
+        self.response.write(render("templates/aluno.html", values))
+
+class StudentsHandler(webapp2.RequestHandler):
+    def get(self):
+        values = {
+            "active": "alunos",
+        }
+        self.response.write(render("templates/alunos.html", values))
+
+class TeacherHandler(webapp2.RequestHandler):
+    def get(self, id):
+        values = {
+            "active": "professores",
+        }
+        self.response.write(render("templates/aluno.html", values))
+
+class TeachersHandler(webapp2.RequestHandler):
+    def get(self):
+        values = {
+            "active": "professores",
+        }
+        self.response.write(render("templates/alunos.html", values))
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import webapp2
-from controllers import HomeHandler, handler_404
+from controllers import ClassesHandler, ClassHandler, HomeHandler, SettingsHandler, StudentHandler, StudentsHandler, TeacherHandler, TeachersHandler
+from controllers import handler_404, handler_500
 
 app = webapp2.WSGIApplication([
     ('/(?:home)?', HomeHandler),
+    ('/alunos', StudentsHandler),
+    ('/aluno/(\w+)', StudentHandler),
+    ('/professores', TeachersHandler),
+    ('/professor/(\w+)', TeacherHandler),
+    ('/turmas', ClassesHandler),
+    ('/turma/(\w+)', ClassHandler),
+    ('/configuracoes', SettingsHandler),
 ], debug=True)
 
 app.error_handlers[404] = handler_404
+app.error_handlers[500] = handler_500
