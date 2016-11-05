509c046325c2423f2f8271e402a8967f7995810c
Felipe Pereira <felipe.croata@gmail.com>
2016-11-05 05:31:15 -0200

    Deduplicating code in settings html and js.

--- static/js/configuracoes.js
+++ static/js/configuracoes.js
@@ -21,13 +21,37 @@ $(function(){
         );
     }
 
-    function changeTo($indicator, type, color, newTooltip) {
-        $indicator.css({transform: "rotate(0deg)", color: color})
+    function changeTo($span, type, color, newTooltip) {
+        $span.css({transform: "rotate(0deg)", color: color})
                   .removeClass("glyphicon-refresh")
                   .addClass("glyphicon-" + type)
                   .tooltip("hide")
                   .attr("data-original-title", newTooltip)
                   .tooltip("fixTitle");
+    }
+
+    function createIndicator($statusCell) {
+        var $span = $(
+            '<span class="glyphicon glyphicon-refresh" aria-hidden="true" data-html="true"' +
+            'data-toggle="tooltip" data-placement="top" title="Sincronizando...">' +
+            '</span>');
+        $statusCell.html($span);
+        $span.tooltip();
+        return {
+            span: $span,
+            rotation: rotateForEver($span)
+        };
+    }
+
+    function indicatorToRedCross(indicator) {
+        return function(jqXHR, textStatus, errorThrown) {
+            indicator.rotation.stop();
+            changeTo(
+                indicator.span,
+                "remove",
+                "red",
+                "Error: " + jqXHR.status + " " + errorThrown + "<br/>(" + jqXHR.responseText + ")");
+        };
     }
 
     function pencilClickHandler() {
@@ -42,22 +66,23 @@ $(function(){
         $emailCell.html($input);
         $input.select();
 
+        function reset() {
+            if (isNew) {
+                $tr.remove();
+            } else {
+                $emailCell.html(oldEmail);
+            }
+        }
+
         $input.keyup(function(event){
             switch (event.which) {
                 case 13: // Key: Enter
                     var newEmail = $input.val();
                     $emailCell.html(newEmail);
 
-                    // TODO:
-                    //  1) enclose the two spans below in a div (this will help with the animation)
-                    //  2) create a hidden html element with id="template", and clone it
-                    var $indicator = $(
-                        '<span class="glyphicon glyphicon-refresh" aria-hidden="true"' +
-                        'data-toggle="tooltip" data-placement="right" title="Sincronizando...">' +
-                        '</span>');
-                    $statusCell.html($indicator);
-                    $indicator.tooltip();
-                    var $rotation = rotateForEver($indicator);
+                    var indicator = createIndicator($statusCell);
+                    var $span = indicator.span;
+                    var $rotation = indicator.rotation;
 
                     $.post({
                         url: "/ajax/emails/" + (isNew ? "add" : "edit"),
@@ -71,80 +96,38 @@ $(function(){
                             }
 
                             $rotation.stop();
-                            changeTo($indicator, "ok", "green", "Sucesso");
-                            $indicator.delay(3000).fadeOut(300, $.fn.remove.bind($indicator));
+                            changeTo($span, "ok", "green", "Sucesso");
+                            $span.delay(3000).fadeOut(300, $.fn.remove.bind($span));
                         },
-                        error: function(jqXHR, textStatus, errorThrown){
-                            $rotation.stop();
-                            changeTo(
-                                $indicator,
-                                "remove",
-                                "red",
-                                "Erro: " + jqXHR.status + " " + errorThrown);
-                            $emailCell.html(oldEmail);
-                        }
+                        error: [indicatorToRedCross(indicator), reset]
                     });
                     break;
                 case 27: // Key: Esc
-                    if (isNew) {
-                        $tr.remove();
-                    } else {
-                        $emailCell.html(oldEmail);
-                    }
+                    reset();
                     break;
             }
         });
     }
 
     function trashClickHandler() {
-        var $statusCell = $(this).parent();
-        var $tr = $statusCell.parent();
-        var id = $tr.data("id");
-        var email = $tr.children().first().text();
+        var $tr = $(this).parents("tr");
+        var indicator = createIndicator($tr.children().last());
 
-        // TODO: same as above
-        $statusCell.append(
-            '<span>&nbsp;&nbsp;&nbsp;</span>' +
-            '<span class="glyphicon glyphicon-refresh" aria-hidden="true"' +
-            'data-toggle="tooltip" data-placement="right" title="Sincronizando...">' +
-            '</span>');
-        var $indicator = $statusCell.children().last().tooltip();
-        var $rotation = rotateForEver($indicator);
         $.post({
             url: "/ajax/emails/remove",
             data: {
-                id: id
+                id: $tr.data("id")
             },
             success: function(data, textStatus, jqXHR){
-                $rotation.stop();
                 $tr.remove();
             },
-            error: function(jqXHR, textStatus, errorThrown){
-                $rotation.stop();
-                changeTo(
-                    $indicator,
-                    "remove",
-                    "red",
-                    "Erro: " + jqXHR.status + " " + errorThrown);
-            }
+            error: indicatorToRedCross(indicator)
         });
     }
 
     function addHandler() {
         var $table = $(this).parents("tbody");
-        // TODO:
-        //  1) create hidden html element with id="template" and clone it
-        //  2) use jinja macro to DRY
-        var $newRow = $(
-            '<tr data-id="new">' +
-            '<td></td>' +
-            '<td>' +
-                '<span class="glyphicon glyphicon-pencil pointer" aria-hidden="true"></span>\n' +
-                '<span>&nbsp;&nbsp;&nbsp;</span>\n' +
-                '<span class="glyphicon glyphicon-trash pointer" aria-hidden="true"></span>' +
-            '</td>' +
-            '<td></td>' +
-            '</tr>');
+        var $newRow = $("#row-template").clone().show();
         $table.children("tr.active").before($newRow);
         $newRow.find(".glyphicon-pencil").click(pencilClickHandler).click();
         $newRow.find(".glyphicon-trash").click(trashClickHandler);
